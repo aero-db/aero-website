93889f045179a589f96baf786c7fc07e1433cb28
Cyriaque <cyriaquedelaunay@gmail.com>
2024-10-20 19:54:30 +0200

    feat: added seo tools

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -7,10 +8,18 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [vue(), vueJsx(), vueDevTools(), sentryVitePlugin({
+    org: "aerodb",
+    project: "javascript-vue"
+  })],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+
+  build: {
+    sourcemap: true
   }
 })
