a8ef62034b77373196216203f8bae89ca114490f
Cyriaque <cyriaquedelaunay@gmail.com>
2024-10-20 17:10:59 +0200

    feat: handle homepage request

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 import express from 'express'
-import path from 'path'
 import { fileURLToPath } from 'url'
 import { dirname } from 'path'
 import cors from 'cors'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
+const path = __dirname + '/app/views/'
 const app = express()
 
-// Enable CORS for all routes
 app.use(cors())
+
+app.use(express.static(path))
+
+app.get('/', function (req, res) {
+  res.sendFile(path + 'index.html')
+})
+
+// Enable CORS for all routes
 
 // Serve static files from the Vue app
 app.use(express.static(path.join(__dirname, 'dist')))
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
 // Handle all other routes with the Vue app
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'))
