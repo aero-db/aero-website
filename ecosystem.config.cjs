d3e6eb44a8073ef98b9e846ebfeb0a92175c3e82
Cyriaque <cyriaquedelaunay@gmail.com>
2024-11-18 14:18:44 +0100

    feat: set port

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
     {
       name: 'AeroDb Website',
       exec_mode: 'cluster',
+      port: '8080',
       instances: 'max',
       script: './.output/server/index.mjs',
     },
