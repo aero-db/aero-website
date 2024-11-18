5311206628f5c9557c7c27bd8d87eba40ae6c3a2
Cyriaque <cyriaquedelaunay@gmail.com>
2024-11-18 17:03:38 +0100

    feat: added splashscreen

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="flex flex-col w-screen h-screen overflow-auto">
+    <div class="fixed z-20 top-0 left-0 w-screen h-screen bg-surface-900 flex items-center justify-center" v-if="showSplash">
+      <Image src="/icon.svg" width="64" alt="AeroDB icon" />
+    </div>
+
     <Topbar />
     <div class="flex-1 mt-16">
       <slot />
@@ -8,6 +12,11 @@
 </template>
 <script setup lang="ts">
 import Topbar from './Topbar.vue';
+
+const showSplash = ref(true);
+onNuxtReady(async () => {
+  showSplash.value = false;
+});
 
 useSeoMeta({
   title: `AeroDB`,
